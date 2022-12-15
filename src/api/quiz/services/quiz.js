7b289d38287dcd38d6e133ad9790e952327a745c
abhishah <abhishah0961@gmail.com>
2022-12-15 16:04:34 +0530

    Added: score method

--- src/api/quiz/services/quiz.js
+++ src/api/quiz/services/quiz.js
@@ -9,7 +9,34 @@ const { createCoreService } = require('@strapi/strapi').factories;
 module.exports = createCoreService('api::quiz.quiz', ({ strapi }) => ({
     async score(ctx) {
         const { id } = ctx.params
-        console.log(id);
-        return id
+        let userAnswers = ctx.request.body
+        const quiz = await strapi.entityService.findOne("api::quiz.quiz", id, {
+            populate: { questions: true }
+        })
+
+        let question
+        let score = 0
+
+        if (quiz) {
+            userAnswers.map((ua) => {
+                question = quiz.questions.find((q) => q.id === ua.questionId)
+                if (question) {
+                    if (question.answer === ua.value) {
+                        ua.correct = true;
+                        score += 1;
+                    } else {
+                        ua.correct = false;
+                    }
+
+                    ua.correctValue = question.answer;
+                }
+
+                return ua;
+            })
+
+        }
+        const questionCount = quiz.questions.length;
+        delete quiz.questions;
+        return { quiz, score, scoredAnswers: userAnswers, questionCount };
     }
 }));
